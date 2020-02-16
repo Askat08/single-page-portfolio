5e06500c2e035daf0ec6fd670ab919ef0b4cfc80
Askat B <Askat@MacBook-Pro-2.home>
2020-02-15 22:06:56 -0500

    comments are deleted

---
+++ js/animatedText.js
@@ -0,0 +1,59 @@
+// function([string1, string2],target id,[color1,color2])
+consoleText(["Web Developer", "Computer Tech", "Self-taught student."], "text");
+
+function consoleText(words, id) {
+  var visible = true;
+  var con = document.getElementById("console");
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id);
+  window.setInterval(function() {
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function() {
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = "console-underscore hidden";
+      visible = false;
+    } else {
+      con.className = "console-underscore";
+
+      visible = true;
+    }
+  }, 400);
+}
+
+// ================= Scroll arrow =======================
+
+$(function() {
+  $("a[href*=#]").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
+  });
+});
+
+// ================== Tooltip ===========================
